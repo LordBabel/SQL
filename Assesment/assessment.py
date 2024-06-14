c6d9cd82fe58fb1e6cf0456133af59c59046b53a
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-06-14 15:23:18 +1200

    Sprint trialing 1 finished custom sql now more readable Will work on feedback

--- Assesment/assessment.py
+++ Assesment/assessment.py
@@ -1,10 +1,40 @@
+'''
+Brawl Stars database application by William Li Chen
+
+'''
+#  imports
 import sqlite3
 
-db = sqlite3.connect("assessment.db")
-conn = db.cursor()
-sql = "SELECT Name, HP FROM Brawlers ORDER BY HP desc;"
-conn.execute(sql)
-result = conn.fetchall()
-print(result)
+#  constants and variables
+DATABASE = "assessment.db"
 
-db.close()
+#  functions
+
+
+def print_all_brawlers():
+    db = sqlite3.connect(DATABASE)
+    conn = db.cursor()
+    sql = "SELECT * FROM Brawlers ORDER BY ID"
+    conn.execute(sql)
+    result = conn.fetchall()    #  loop through results, print it nicely
+    print("ID   Wallbreak   Name           HP      Rarity ID   Year Released   Class ID")
+    for brawler in result:
+        print(f"{brawler[0]:<5}{brawler[1]:<12}{brawler[2]:<15}{brawler[3]:<8}{brawler[4]:<12}{brawler[5]:<16}{brawler[6]:<5}")
+    db.close()
+
+
+
+def print_high_HP():
+    db = sqlite3.connect(DATABASE)
+    conn = db.cursor()
+    sql = "SELECT Name, HP FROM Brawlers ORDER BY HP desc;"
+    conn.execute(sql)
+    result = conn.fetchall()    #  loop through results, print it nicely
+    print("Brawler        HP")
+    for brawler in result:
+        print(f"{brawler[0]:<15}{brawler[1]:<5}")
+    db.close()
+
+
+#  main
+print_all_brawlers()
